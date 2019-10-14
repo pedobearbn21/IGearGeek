9d95039403a05c91f8f6db6d72ea3bbe596ca777
pedobearbn21 <pedobearearn@gmail.com>
2019-10-14 14:56:07 +0700

    setup-laravel

--- Laravel/app/Models/User.php
+++ Laravel/app/Models/User.php
@@ -1,14 +1,17 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
+use Laratrust\Traits\LaratrustUserTrait;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use LaratrustUserTrait;
+    use HasApiTokens, Notifiable;
 
     /**
      * The attributes that are mass assignable.
