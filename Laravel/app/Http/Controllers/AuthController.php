e01a3559b267f6c788c3fd178640cfd83527e238
pedobearbn21 <pedobearearn@gmail.com>
2019-10-19 18:08:06 +0700

    [Edited] Oauth

--- Laravel/app/Http/Controllers/AuthController.php
+++ Laravel/app/Http/Controllers/AuthController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
-use Carbon\Carbon;
 use Validator;
 
 class AuthController extends Controller
@@ -18,14 +17,15 @@ class AuthController extends Controller
                      'c_password' => 'required|same:password',
            ]);
         if ($validator->fails()) {
-            return response()->json(['error'=>$validator->errors()], 401);                        }
+            return response()->json(['error'=>$validator->errors()], 401);
+        }
         $user = new User([
             'name' => $request->name,
             'email' => $request->email,
             'password' => bcrypt($request->password)
         ]);
         $user->save();
-        return response()->json(['success'=>$user], 200);
+        return $user;
        }
 
 
@@ -33,8 +33,8 @@ class AuthController extends Controller
     public function login(){
         if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){
             $user = Auth::user();
-            $success['token'] =  $user->createToken('MyApp')-> accessToken;
-            return response()->json(['success' => $success], $this-> successStatus);
+            $success['token'] =  $user->createToken('token')-> accessToken;
+            return response()->json(['user' => $user, 'token' => $success], 200);
         }
         else{
             return response()->json(['error'=>'Unauthorised'], 401);
